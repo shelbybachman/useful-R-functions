59614272b70a478156d07283c4743630afce41e7
Shelby Bachman <11279603+shelbybachman@users.noreply.github.com>
2020-08-25 19:10:09 -0700

    update check_outliers function

--- check-outliers.R
+++ check-outliers.R
@@ -1,38 +1,42 @@
-check_outliers <- function(data, var) {
+check_outliers <- function(data, name, colour = '#6ec499') {
   # this function detects outliers
   # using the MAD-median rule,
   # and shows a plot with outliers
   # based on standard boxplot rule.
-  # dependencies: ggplot2;
+  # dependencies: ggplot2, ggpubr
   #
   # arguments:
-  # data - dataframe containing variable,
-  # var1 - string, name of variable
+  # data - variable of interest, a numeric vector
+  # name - name of variable, a string (goes in figure title)
+  # colour - hex code for fill of boxplot, default #40a070
   
-  sample_median <- median(data[,var], na.rm = TRUE)
-  sample_mad <- mad(data[,var], na.rm = TRUE)
-  data[,'mad_med'] <- (data[,var] - sample_median) / (1.483 * sample_mad)
-  data[,'outlier'] <- data[,'mad_med'] > 2.24
+  sample_median <- median(data, na.rm = TRUE)
+  sample_mad <- mad(data, na.rm = TRUE)
+  mad_med <- ifelse(is.na(data), NA, data - sample_median / (1.483 * sample_mad))
+  outliers <- ifelse(mad_med > 2.24, 1, 0)
   
   output <- vector('list', 2)
-  output[[1]] <- which(data[,'outlier'] == TRUE)
+  output[[1]] <- which(outliers == 1)
   if (length(output[[1]]) == 0) {
     message <- paste('MAD-median rule detected ', 
-                     sum(data[,'outlier'] == TRUE), 
+                     sum(outliers == 1), 
                      ' outliers', sep = '')
   } else {
     message <- paste('MAD-median rule detected ', 
-                     sum(data[,'outlier'] == TRUE), 
+                     sum(outliers == 1), 
                      ' outlier(s) ', sep = '')
   }
   
-  output[[2]] <- ggplot2::ggplot(data = data,
-                                 ggplot2::aes_string(y = var)) +
-    ggplot2::geom_boxplot(fill = '#6ec499', alpha = 0.5, width = 0.3,
+  data_plot <- as.data.frame(data)
+  output[[2]] <- ggplot2::ggplot(data = data_plot,
+                                 ggplot2::aes(y = data)) +
+    ggplot2::geom_boxplot(fill = colour, alpha = 0.5, width = 0.3, position = ggplot2::position_dodge(width = 0.5),
                           outlier.size = 3) +
-    ggplot2::labs(x = '', subtitle = message) +
+    ggplot2::labs(x = '', y = name, subtitle = message) +
     ggpubr::theme_pubr() +
-    ggplot2::theme(axis.ticks.x = ggplot2::element_blank(),
+    ggplot2::theme(axis.ticks.y = ggplot2::element_blank(),
+                   axis.text.y = ggplot2::element_blank(),
+                   axis.ticks.x = ggplot2::element_blank(),
                    axis.text.x = ggplot2::element_blank())
   return(output)
 }
